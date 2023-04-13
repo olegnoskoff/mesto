733cb0e1e734d9ebab87734f15a4e5b33314f23f
Oleg <olegnoskoff@yandex.ru>
2023-04-13 14:38:58 +0300

    fix

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -1,6 +1,5 @@
 class FormValidator {
   constructor(settings, formElement) {
-    this._formSelector = settings.formSelector;
     this._inputSelector = settings.inputSelector;
     this._inactiveButtonClass = settings.inactiveButtonClass;
     this._inputErrorClass = settings.inputErrorClass;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { FormValidator } from "../components/FormValidator.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
+import initialCards from "../utils/constants.js";
 
 const settings = {
   formSelector: ".popup__form",
@@ -15,34 +16,7 @@ const settings = {
   errorClass: "popup__error_visible",
 };
 
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-
-const UserInfoController = new UserInfo(".profile__name", ".profile__about");
+const userInfoController = new UserInfo(".profile__name", ".profile__about");
 
 const cardPopup = document.querySelector("#imageCard");
 const cardPopupCity = cardPopup.querySelector(".popupimage__title");
@@ -67,10 +41,6 @@ const cardAddBtn = document.querySelector(".profile__add-button");
 
 const cardAddPopup = document.querySelector("#popup-add");
 const cardAddPopupForm = cardAddPopup.querySelector(".popup__form");
-const cityInput = cardAddPopup.querySelector(".popup__input_type_city");
-const linkInput = cardAddPopup.querySelector(".popup__input_type_link");
-
-const popups = document.querySelectorAll(".popup");
 
 const validatorProfile = new FormValidator(settings, profilePopup);
 const validatorEdit = new FormValidator(settings, cardAddPopup);
@@ -79,25 +49,32 @@ validatorProfile.enableValidation();
 validatorEdit.enableValidation();
 
 //вызывается при клике на карточку
-function handleCardClick(card) {
+function handleCardClick(name, link) {
   let popupWithImage = new PopupWithImage(".popupimage");
-  popupWithImage.open(card.name, card.link);
+  popupWithImage.open(name, link);
 }
 
 let cards = [];
 const cardsContainerSelector = ".elements";
-const cardRender = (card) => {
+const renderCard = (name, link)=> {
+  const card = new Card(renderCard, name, link, "#template", handleCardClick);
   return card.render();
-};
+}
+
 initialCards.forEach((item) => {
   cards.push(new Card(item.name, item.link, "#template", handleCardClick));
 });
 
 const cardsSection = new Section(
-  { items: cards, render: cardRender },
+  { 
+    items: initialCards, 
+    render: (renderCard) =>{
+      const card = new Card(renderCard, "#template", handleCardClick);
+      return card.generateCard()
+     },
   cardsContainerSelector
-);
-cardsSection.renderItems();
+    });
+// cardsSection.renderItems();
 
 //Add form
 const submitAddCallback = (data) => {
@@ -116,7 +93,7 @@ function handleAddClick() {
 }
 
 const submitProfileCallback = (data) => {
-  UserInfoController.setUserInfo(data["profile-name"], data["profile-about"]);
+  userInfoController.setUserInfo(data["profile-name"], data["profile-about"]);
 };
 function handleProfileEdit() {
   const popupWithForm = new PopupWithForm("#popup-edit", submitProfileCallback);
