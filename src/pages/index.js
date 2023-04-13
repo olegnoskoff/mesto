fc55736b743e40c0154879c8b6109a8f3cd32181
Oleg <olegnoskoff@yandex.ru>
2023-04-13 13:17:46 +0300

    changed the folder structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import "../pages/index.css";
-import Section from "./Section.js";
-import { Card } from "./Card.js";
-import { FormValidator } from "./FormValidator.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+import Section from "../components/Section.js";
+import { Card } from "../components/Card.js";
+import { FormValidator } from "../components/FormValidator.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
 
 const settings = {
   formSelector: ".popup__form",
