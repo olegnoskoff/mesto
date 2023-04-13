679fd6ad919ce92a0790f1cbb229d41bad1f680c
Oleg <olegnoskoff@yandex.ru>
2023-04-13 13:19:26 +0300

    changed the path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // подклю
 
 module.exports = {
   // Точка входа
-  entry: { main: "./src/scripts/index.js" },
+  entry: { main: "./src/pages/index.js" },
 
   // Точка выхода
   output: {
