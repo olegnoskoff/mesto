b272c80826e732be9ea2a8980f898871030d1b91
Oleg <olegnoskoff@yandex.ru>
2023-04-13 13:09:37 +0300

    fixed the placement in folders

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,18 @@ module.exports = {
 
       // Изображения и шрифты
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource", // переносить исходные файлы в конечную сборку в том же формате
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[hash][ext]",
+        },
       },
 
       // MiniCssExtractPlugin.loader и css-loader
